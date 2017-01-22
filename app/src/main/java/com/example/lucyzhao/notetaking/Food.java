8d1be1d26dad338efcec0457d381db210d8fa746
Luxi Zhao <lucyzhao.zlx@gmail.com>
2017-01-21 21:42:31 -0800

    saved array list to memory, ensures unique recipe name

--- app/src/main/java/com/example/lucyzhao/notetaking/Food.java
+++ app/src/main/java/com/example/lucyzhao/notetaking/Food.java
@@ -1,9 +1,11 @@
 package com.example.lucyzhao.notetaking;
 
+import java.io.Serializable;
+
 /**
  * Created by LucyZhao on 2016/10/29.
  */
-public class Food {
+public class Food implements Serializable{
     private final String title;
 
     public Food( String title ) {
