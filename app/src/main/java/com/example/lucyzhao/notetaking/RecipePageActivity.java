f7bc563185ac6e52c123218dcc40385e9a80b617
Luxi Zhao <lucyzhao.zlx@gmail.com>
2017-03-02 22:09:33 -0800

    added bullet points

--- app/src/main/java/com/example/lucyzhao/notetaking/RecipePageActivity.java
+++ app/src/main/java/com/example/lucyzhao/notetaking/RecipePageActivity.java
@@ -2,16 +2,24 @@ package com.example.lucyzhao.notetaking;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ActionMenuView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +32,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Stack;
+
+import static android.view.Gravity.CENTER;
 
 public class RecipePageActivity extends AppCompatActivity {
     EditText ingredientsEditText;
@@ -31,6 +42,13 @@ public class RecipePageActivity extends AppCompatActivity {
     private String file_name;
     private static final String CHILD_PATH_INGREDIENTS = "_ingredients";
     private static final String CHILD_PATH_PROCEDURE = "_procedure";
+    private float density;
+    /* ingredient fields */
+    private int ingredient_prevNumOfLines = 1;
+    private Stack<Integer> ingredient_bulletIds = new Stack<>();
+    /* procedure fields */
+    private int procedure_prevNumOfLines = 1;
+    private Stack<Integer> procedure_bulletIds = new Stack<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,9 +69,10 @@ public class RecipePageActivity extends AppCompatActivity {
         foodPic.setImageBitmap(bmp);
 
         /* -------------find EditText contents -------------*/
-        procedureEditText = (EditText) findViewById(R.id.procedure);
-        ingredientsEditText = (EditText) findViewById(R.id.ingredients);
-
+        procedureEditText = (EditText) findViewById(R.id.procedure_edit_text);
+        ingredientsEditText = (EditText) findViewById(R.id.ingredients_edit_text);
+        final RelativeLayout ingredientsRL = (RelativeLayout) findViewById(R.id.ingredients_relative_layout);
+        final RelativeLayout procedureRL = (RelativeLayout) findViewById(R.id.procedure_relative_layout);
         //read information saved in internal storage and displays it
         displayInfo();
 
@@ -64,7 +83,97 @@ public class RecipePageActivity extends AppCompatActivity {
             Log.v("internal str files", f.getAbsolutePath());
         }
         /* ---------------test display all files-------------*/
+
+
+        /* ----------deal with ingredients edit text----------*/
+        density = getApplicationContext().getResources().getDisplayMetrics().density;
+        ingredient_bulletIds.push(R.id.first_bullet);
+
+        ingredientsEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int numOfLines = ingredientsEditText.getLineCount();
+                Log.v("line num", numOfLines+" lines");
+                if(numOfLines > ingredient_prevNumOfLines){
+                    RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                   // lp.setMarginStart((int)(10*density));
+                    lp.addRule(RelativeLayout.BELOW, ingredient_bulletIds.peek());
+                    lp.addRule(RelativeLayout.ALIGN_START, ingredient_bulletIds.peek());
+                    lp.width = (int)(10*density);
+                    lp.height = (int)(10*density);
+                    lp.topMargin = (int)(ingredientsEditText.getTextSize() + ingredientsEditText.getLineSpacingExtra() - lp.height/2 -8 );
+
+                    ImageView bullet = new ImageView(RecipePageActivity.this);
+                    bullet.setBackgroundResource(R.drawable.dot);
+                    bullet.setLayoutParams(lp);
+                    int lastBulletId = View.generateViewId();
+                    ingredient_bulletIds.push(lastBulletId);
+                    bullet.setId(lastBulletId);
+                    ingredientsRL.addView(bullet);
+                }
+                else if(numOfLines < ingredient_prevNumOfLines){
+                    View bullet = ingredientsRL.findViewById(ingredient_bulletIds.peek());
+                    ingredientsRL.removeView(bullet);
+                    ingredient_bulletIds.pop();
+                }
+                ingredient_prevNumOfLines = numOfLines;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        procedure_bulletIds.push(R.id.second_bullet);
+        procedureEditText.addTextChangedListener(new TextWatcher(){
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int numOfLines = procedureEditText.getLineCount();
+                Log.v("line num", numOfLines+" lines");
+                if(numOfLines > procedure_prevNumOfLines){
+                    RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                    // lp.setMarginStart((int)(10*density));
+                    lp.addRule(RelativeLayout.BELOW, procedure_bulletIds.peek());
+                    lp.addRule(RelativeLayout.ALIGN_START, procedure_bulletIds.peek());
+                    lp.width = (int)(10*density);
+                    lp.height = (int)(10*density);
+                    lp.topMargin = (int)(procedureEditText.getTextSize() + procedureEditText.getLineSpacingExtra() - lp.height/2 -8 );
+
+                    ImageView bullet = new ImageView(RecipePageActivity.this);
+                    bullet.setBackgroundResource(R.drawable.dot);
+                    bullet.setLayoutParams(lp);
+                    int lastBulletId = View.generateViewId();
+                    procedure_bulletIds.push(lastBulletId);
+                    bullet.setId(lastBulletId);
+                    procedureRL.addView(bullet);
+                }
+                else if(numOfLines < procedure_prevNumOfLines){
+                    View bullet = procedureRL.findViewById(procedure_bulletIds.peek());
+                    procedureRL.removeView(bullet);
+                    procedure_bulletIds.pop();
+                }
+                procedure_prevNumOfLines = numOfLines;
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
+
 
     /**
      * save user input 
